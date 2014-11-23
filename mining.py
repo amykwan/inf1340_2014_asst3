9e9fdd8fc323fc7db00ce722d3101bf1765bd586
eugenekang <eugene.yc.kang@gmail.com>
2014-11-23 16:24:45 -0500

    - Merged mining_euge.py and mining.py - Merged test_mining_euge.py and test_mining.py

--- mining.py
+++ mining.py
@@ -8,151 +8,155 @@ __license__ = "MIT License"
 
 __status__ = "Prototype"
 
-# imports one per line
 import json
-import datetime
 import unicodedata
+
 stock_data = []
 monthly_averages = []
+result = []
 current_stock = ""
 
 
 def read_stock_data(stock_name, stock_file_name):
     """
-
-    :param stock_name:
-    :param stock_file_name:
-    :return:
+    This function reads in the entirety of the stock data, decodes it, and
+    separates it into two lists of tuples: a list containing total sales by
+    month with the equation (Close Price * Volume), and another containing
+    total volume of stock sold by month. These values are then used to
+    calculate the average price of stock sold via the formula:
+        Avg Stock Sold = Monthly Sales / Monthly Volume
+    :param stock_name: this parameter is required, but unused in this
+        iteration.
+    :param stock_file_name: Input JSON file
+    :return: unused in this iteration.
     """
-
-    """
-        IMO there's no need for a helper function. Just use read stock data.
-        Of note: I changed the names of the variables so its more
-        english-readable. I was having difficulty parsing the different variables
-        when they were all some variation of the word "month".
-        Overall the code you wrote for the processing of the data was really good!
-        It looks like I changed it but really its just formatting, minor tweaks,
-        and making the variable names more differentiated. - Eugene
-    """
-    # Decode the JSON file to object
+    # Decode JSON to object
     stock_info = read_json_from_file(stock_file_name)
 
-    # Initialize variables, list, dict
-    # In format YYYY-MM
-    timestamp = ""
-
-    # List of dates [YYYY-MM] that have stock volume and total sales information
+    # Create lists for storing data
     unique_date_list = []
     monthly_volume = {}
     monthly_sales = {}
 
-    # Loop: gathers all the information from ever entry in stock_info
-    # creates 2 dict with a key-value pair of [timestamp][value]
     for entry in stock_info:
-        # Take month and year from date in format "YYYY-MM"
-        # Somewhere in this structure it is assigning MULTIPLE keys to the same value - ex:
-        # There are multiple keys with the value "2007-10" for some reason. Probably python
-        # is not able to tell if the date written is unique, maybe it's not comparing strings
-        # but is instead comparing memory locations
+        # Create a timestamp in format "YYYY-MM"
         timestamp = entry["Date"][0:7]
 
-        # The unique_date_list structure is actually not used...
-        #if timestamp not in unique_date_list:
-        #    unique_date_list.append(timestamp)
+        # Check if timestamp already exists, if not add to list
+        if timestamp not in unique_date_list:
+            unique_date_list.append(timestamp)
 
-        # Puts the volume and total sales into seperate dicts
+        # Check if timestamp is already a key in the monthly_volume list,
+        # if not add.
         if timestamp not in monthly_volume.keys():
-            # Establishes key-value pairs
             monthly_volume[timestamp] = entry["Volume"]
 
+        # Check if timestamp is already a key in the monthly_sales list,
+        # if not add.
         if timestamp not in monthly_sales.keys():
-            # Establishes key-value pairs
-            monthly_sales[timestamp] = \
+            monthly_sales[timestamp] = entry["Volume"] * entry["Close"]
+
+        # Total value of sales and volume into lists
+        else:
+            monthly_volume[timestamp] = monthly_volume[timestamp] + \
+                entry["Volume"]
+            monthly_sales[timestamp] = monthly_sales[timestamp] + \
                 entry["Volume"] * entry["Close"]
 
-        else:
-            # Adds to key-value pairs
-            # += This does not work with lists, you needs to do:
-            # l = []
-            # l = l + x
-            monthly_volume[timestamp] = monthly_volume[timestamp] + entry["Volume"]
-            monthly_sales[timestamp] = \
-                monthly_sales[timestamp] + (entry["Volume"] * entry["Close"])
-
-        # Append all information into list of tuples.
-    for date in monthly_sales.keys():
+    # Calculate average monthly sales, and change the date format to "YYYY/MM"
+    # Append to the monthly_averages list
+    for date in unique_date_list:
         avg_monthly_sales = round(monthly_sales[date]/monthly_volume[date], 2)
-        month_byte = unicodedata.normalize('NFKD', date).encode('ascii','ignore')
+        month_byte = unicodedata.normalize('NFKD', date).encode('ascii',
+                                                                'ignore')
         month_string = month_byte.decode("utf-8")
         month_string = month_string.replace("-", "/")
         monthly_averages.append((month_string, avg_monthly_sales))
-
+    return
 
 
 def six_best_months():
-    return get_six_months_by_decision("+")
+    """
+    This function produces the best six months of average stock price.
+    :return: The best six months of average stock price in a list of tuples, in
+    the format('YYYY/DD', 111.11)
+    """
+    result = validate_averages()
+    # If data was validated, sort and order from highest to lowest starting at
+    # index 0
+    if "Validated" in result:
+        sorted_best_avg = (sorted(monthly_averages, key=lambda tup: tup[1],
+                                  reverse=True))
+        return sorted_best_avg[0:6]
+
+    # If data was not validated, return the result passed to this function.
+    else:
+        return result
+
 
 def six_worst_months():
-    return get_six_months_by_decision("-")
+    """
+    This function produces the worst six months of average stock price.
+    :return: The worst six months of average stock price in a list of tuples,
+    in the format ('YYYY/DD', 111.11)
+    """
+    result = validate_averages()
+    # If data was validated, sort and order from lowest to highest starting at
+    # index 0
+    if "Validated" in result:
+        sorted_worst_avg = (sorted(monthly_averages, key=lambda tup: tup[1],
+                                   reverse=False))
+        return sorted_worst_avg[0:6]
 
-#Get the six best/worst month base on the parameter, this is a helper function
-"""
-    This is going to take me some time to read over. Has bubble-sort
-    (or any other sorting methods) been covered in class yet?
-    I'm hoping there's an easier way to do it, but if not, this looks like it should work!
-"""
-def get_six_months_by_decision(decision):
+    # If data was not validated, return the result passed to this function.
+    else:
+        return result
+
+
+def validate_averages():
+    """
+    This function validates the monthly averages list to ensure that it has a
+    full range of entries to sort through. If not, it will either return all
+    zero values, or it will take what values it can and enter it into a list,
+    and then return zero values.
+    :return: If the data is incomplete, a list of six months, if data is
+     complete it will
+    insert the string "Validated" into the list and return that.
+    """
     result = []
-    #If the monthly_averages is empty, return a list of empty tuples
-    if monthly_averages == [] or decision not in "+-":
-        result = [('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0)]
 
-    #Return all the avaiable day averages if the length of the list is less than or equal to 6
+    # If monthly averages is empty, return a "blank" result.
+    if monthly_averages is []:
+        result = [('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0),
+                  ('', 0.0)]
+
+    # If the monthly averages data is incomplete (less than 6) fill the
+    # remaining empty slots with zeroes
     elif len(monthly_averages) <= 6:
-        for month_history in monthly_averages:
-            result.append(month_history)
+        for entry in monthly_averages:
+            result.append(entry)
         for count in range(6 - len(monthly_averages)):
             result.append(('', 0.0))
 
-    #Check the decision and perform different acts
+    # Otherwise return validated
     else:
-        #by here have checked that there is 6 months of data
-        #Load the first 6 averages
-        for count in range(6):
-            result.append(monthly_averages[count])
-        #Compare the rest of the averages with the six averages in the result list
-        for month_history in monthly_averages[6:]:
-            for count in range(6):
-                #If decision equals to "+", then perform the act of finding the best months
-                if decision == "+":
-                    #Exchange the one on the result list if the average is higher than it
-                    if result[count][1] < month_history[1]:
-                        old_result = (result[count][0], result[count][1])
-                        result[count] = month_history
-                        month_history = old_result
-                #If decision equals to "+", then perform the act of finding the worst months
-                elif decision == "-":
-                    #Exchange the one on the result list if the average is lower than it
-                    if result[count][1] > month_history[1]:
-                        old_result = (result[count][0], result[count][1])
-                        result[count] = month_history
-                        month_history = old_result
+        result.append("Validated")
     return result
+
 
 def read_json_from_file(file_name):
     """
     Reads and decodes .json file for use.
 
-    :param file_name: The name of the JSON formatted file that contains stock prices
+    :param file_name: The name of the JSON formatted file that contains
+    stock prices
     :return: Decoded JSON object
     """
-    # use a try-catch block when accessing files to ensure that if the file is not found
-    # an exception will be thrown.
-    # Note: the logic of your statement is fine, its just good to have this. - Eugene
+
     try:
         with open(file_name) as file_handle:
             file_contents = file_handle.read()
     except FileNotFoundError:
-           raise FileNotFoundError("File Not Found!")
+        raise FileNotFoundError("File Not Found!")
 
     return json.loads(file_contents)
--- test_mining.py
+++ test_mining.py
@@ -14,13 +14,11 @@ from mining import *
 
 def test_goog():
     read_stock_data("GOOG", "data\GOOG.json")
-    # Note - the assertions will test position 0 of the assertion's list to position 0 of our program's list. Meaning
-    # that even if we generate the correct results, if they aren't in the right order the tests will all fail. We need
-    # to address this.
-    assert six_best_months() == [('2007/12', 693.76), ('2007/11', 676.55), ('2007/10', 637.38), ('2008/01', 599.42),
-                                 ('2008/05', 576.29), ('2008/06', 555.34)]
-    # The six worst months is incorrect, its missing 2004/08 which has an average sale of 104.66. Adding it, and
-    # and dropping the highest month from this set.
-    assert six_worst_months() == [('2004/08', 104.66), ('2004/09', 116.38), ('2004/10', 164.52), ('2004/11', 177.09), ('2004/12', 181.01),
-                                  ('2005/03', 181.18)]
 
+    assert six_best_months() == [('2007/12', 693.76), ('2007/11', 676.55),
+                                 ('2007/10', 637.38), ('2008/01', 599.42),
+                                 ('2008/05', 576.29), ('2008/06', 555.34)]
+
+    assert six_worst_months() == [('2004/08', 104.66), ('2004/09', 116.38),
+                                  ('2004/10', 164.52), ('2004/11', 177.09),
+                                  ('2004/12', 181.01), ('2005/03', 181.18)]
