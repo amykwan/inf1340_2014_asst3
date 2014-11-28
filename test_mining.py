b0342f33dc93df9eb4441b76d5b352bf481aef88
amykwan <amykwan.cma@gmail.com>
2014-11-27 23:32:53 -0500

    Updated test file, one of the answers needed updating.

--- test_mining.py
+++ test_mining.py
@@ -6,7 +6,7 @@ __email__ = "ses@drsusansim.org"
 __copyright__ = "2014 Susan Sim"
 __license__ = "MIT License"
 
-__status__ = "v3"
+__status__ = "v4"
 
 # imports one per line
 import pytest
@@ -39,14 +39,15 @@ def test_overlapping_sets():
 
 
 #tests a situation in which only four months' of data is supplied - currently fails
-# def test_too_few_months():
-#     read_stock_data("TOOFEW", "data\TOOFEW.json")
-#
-#     assert six_best_months() == [('', 0.0), ('', 0.0), ('', 0.0),
-#                                  ('', 0.0), ('', 0.0), ('', 0.0)]
-#
-#     assert six_worst_months() == [('', 0.0), ('', 0.0), ('', 0.0),
-#                                   ('', 0.0), ('', 0.0), ('', 0.0)]
+def test_too_few_months():
+    read_stock_data("TOOFEW", "data\TOOFEW.json")
+
+    assert six_best_months() == [('2003/05', 44.67), ('2003/06', 44.82), ('2003/07', 44.3),
+                                 ('2003/08', 44.63), ('', 0.0), ('', 0.0)]
+
+    assert six_worst_months() == [('2003/05', 44.67), ('2003/06', 44.82), ('2003/07', 44.3), ('2003/08', 44.63),
+                                  ('', 0.0), ('', 0.0)]
+
 
 #tests data in non-chronological order
 def test_non_chrono_order():
@@ -63,16 +64,17 @@ def test_non_chrono_order():
 
 #tests duplicate monthly averages. In this case, both Dec 03 and Jan 04 return 44.45. As per her instructions,
 #the most recent date should be returned, but currently it's the earlier date
-# def test_duplicate_values():
-#     read_stock_data("DUPVAL", "data/DUPVAL.json")
-#
-#     assert six_best_months() == [('2004/03', 45.18), ('2004/04', 45.02),
-#                                  ('2003/06', 44.82), ('2003/11', 44.74),
-#                                  ('2004/05', 44.71), ('2003/05', 44.67)]
-#
-#     assert six_worst_months() == [('2003/09', 44.15), ('2004/02', 44.28),
-#                                   ('2003/07', 44.30), ('2004/01', 44.45), #the second set in this row
-#                                   ('2003/10', 44.61), ('2003/08', 44.63)]
+def test_duplicate_values():
+    read_stock_data("DUPVAL", "data/DUPVAL.json")
+
+    assert six_best_months() == [('2004/03', 45.18), ('2004/04', 45.02),
+                                 ('2003/06', 44.82), ('2003/11', 44.74),
+                                 ('2004/05', 44.71), ('2003/05', 44.67)]
+
+    #KELVIN： The original answer you put there was wrong, since there is a smaller set compares to the old answer
+    assert six_worst_months() == [('2003/09', 44.15), ('2004/02', 44.28), ('2003/07', 44.3), ('2003/12', 44.45),
+                                  ('2004/01', 44.45), ('2003/10', 44.61)]
+
 
 def test_no_file_found():
     with pytest.raises(FileNotFoundError):
